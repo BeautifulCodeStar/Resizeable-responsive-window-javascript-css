f2d9b00f78e77e7ed96aefeee20fcefb4f249715
best service <apple930108@gmail.com>
2018-06-26 01:11:54 +0800

    push

---
+++ test.php
@@ -0,0 +1,75 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-120534241-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <!-- End Google Analytics -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <title>Design</title>
+    </head>
+    
+    
+    <script type="text/javascript" src="assets/js/interact.min.js"></script>
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+    
+      <body onload="start()">
+    <div class="resize-container" onclick="stopAnimation()">
+            <div class="resize-drag">
+                <div class="iphone">
+                    <img class="iphone-img animation img-fluid" src="assets/imgs/phone.png" alt="phone" />
+                    <div class="first-desc description">
+                        <div class="point" align="center">
+                            <div class="circle">
+                                <span class="circle-text">1</span>
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text">
+                               PON ACÁ TU CELULAR
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="line-bar"></div>
+                <div class="description">
+                    <div class="point" align="center">
+                        <div class="circle">
+                            <span class="circle-text">2</span>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="text">
+                            AJUSTA EL TAMAÑO DEL ÁREA NARANJA<br> AL TAMAÑO DE LA PANTALLA DE TU CECULAR.
+                        </p>
+                    </div>
+                </div>
+                
+                <div class="resize-btn animation-btn" onclick="stopAnimation()">
+                    <img src="assets/imgs/resize.png">
+                </div>
+            </div>
+        </div>
+
+        <div class='start-block'>
+            <button class='start-btn'>iLISTO!</button>
+        </div>
+
+
+    </body>
+</html>
